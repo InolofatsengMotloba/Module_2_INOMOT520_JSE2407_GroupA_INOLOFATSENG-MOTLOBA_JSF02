109346017eb1f149b6366770d71a9afae0c4fe05
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-07-29 09:18:24 +0200

    Add routing to App.svelte with Home, ProductDetailView, and Login pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,16 @@
 <script>
+  import { Router, Route } from "svelte-routing"
   import Header from "./components/Header.svelte";
+  import Home from "./pages/Home.svelte";
+  import ProductDetailView from "./pages/ProductDetailView.svelte";
+  import Login from "./pages/Login.svelte";
 </script>
 
 <Header />
-<main>
-
-  
-</main>
-
-<style>
-  
-</style>
+<Router>
+  <Route path="/" component={Home} />
+  <Route path="products/:id" component={ProductDetailView} />
+  <Route path="login" component={Login} />
+</Router>
+ 
+<style></style>
