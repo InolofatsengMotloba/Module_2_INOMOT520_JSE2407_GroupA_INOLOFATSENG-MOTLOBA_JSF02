c66b559fc2faeb68b63bcc6fa9ca073b29aa0b4d
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-07-29 14:39:42 +0200

    Add navbar toggle functionality with a button for smaller screens

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,10 @@
-<script></script>
+<script>
+  let isNavbarVisible = false;
+
+  const toggleNavbar = () => {
+    isNavbarVisible = !isNavbarVisible;
+  };
+</script>
 
 <header class="sticky z-50 top-0">
   <nav class="bg-gray-500 border-gray-200">
@@ -11,10 +17,35 @@
           </span>
         </button>
       </a>
-      
+      <button
+        on:click={toggleNavbar}
+        type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+        aria-controls="navbar-dropdown"
+        aria-expanded={isNavbarVisible}
+      >
+        <span class="sr-only">Open main menu</span>
+        <svg
+          class="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
+
       <div
         class="w-full md:block md:w-auto"
         id="navbar-dropdown"
+        class:hidden={!isNavbarVisible}
       >
         <ul class="flex flex-col top-10 font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-500 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
           <li>
