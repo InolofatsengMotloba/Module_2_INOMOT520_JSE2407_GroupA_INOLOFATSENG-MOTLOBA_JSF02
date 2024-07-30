20c4cc6655da6d2e3194f96e3bfccec10a9dabce
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-07-30 02:27:14 +0200

    Define the 'getProducts' Function to Fetch Data

--- src/components/Product/ProductList.svelte
+++ src/components/Product/ProductList.svelte
@@ -6,6 +6,21 @@
   let products = [];
   let loading = true;
   let error = null;
+
+  async function getProducts() {
+  try {
+    let response = await fetch('https://fakestoreapi.com/products');
+    if (!response.ok) {
+      throw new error("Failed to fetch products");
+    }
+    products = await response.json();
+  } catch (err) {
+    error = err.message;
+  } finally {
+    loading = false;
+  }
+}
+
 </script>
 
 <style></style>
