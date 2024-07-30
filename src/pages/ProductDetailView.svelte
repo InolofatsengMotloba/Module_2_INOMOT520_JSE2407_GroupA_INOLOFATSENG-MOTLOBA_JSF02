492bc6603ab96e38c0c9acf96ec6ab4f45c1d7b6
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-07-30 04:45:38 +0200

    Created 'getProductDetails' function to fetch product details by id and handled response errors

--- src/pages/ProductDetailView.svelte
+++ src/pages/ProductDetailView.svelte
@@ -9,6 +9,21 @@
 
   export let id;
 
+  // Function to fetch product details
+  async function getProductDetails(productId) {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/${productId}`);
+
+      if (!response.ok) {
+        throw new error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      return { response: data, error: null };
+    } catch (error) {
+      return { response: null, error: error.message };
+    }
+  }
 </script>
 
 <main>
