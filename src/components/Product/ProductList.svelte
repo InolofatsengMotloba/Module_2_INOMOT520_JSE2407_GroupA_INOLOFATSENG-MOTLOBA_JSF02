f5eb1bb93ca1124b82808198a18c8e357318c6ee
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-07-30 02:26:25 +0200

    Initialize State Variables

--- src/components/Product/ProductList.svelte
+++ src/components/Product/ProductList.svelte
@@ -2,6 +2,10 @@
   import { onMount } from 'svelte';
   import LoadingCard from "./LoadingCard.svelte";
   import Error from "../Error.svelte";
+
+  let products = [];
+  let loading = true;
+  let error = null;
 </script>
 
 <style></style>
